26f32d3f31bd58905fa94381b626619e092f99bc
onkarsarje <73869353+onkarsarje@users.noreply.github.com>
2021-08-25 23:58:26 +0530

    Triangle Quiz

---
+++ quiz.js
@@ -0,0 +1,20 @@
+const quizForm =document.querySelector(".quiz-form");
+const submitAnswerButton=document.querySelector("#submit-answer-btn");
+const outputE1=document.querySelector("#output");
+
+const correctAnswer=["90°","right angled"];
+
+function calculateScore(){
+    let score=0;
+    let index=0;
+    const formResults=new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value === correctAnswer[index]){
+            score++;
+        }
+        index++;
+    }
+    outputE1.innerText = "Your Score is: " + score ;
+}
+
+submitAnswerButton.addEventListener("click", calculateScore);
